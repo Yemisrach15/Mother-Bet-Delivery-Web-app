26da5f83cf98a3412755ca6a8fc8419b822a7db4
Yemisrach Serawit <se.yemisrach.serawit@gmail.com>
2021-02-18 14:33:41 +0300

    user can add to favorite, choose from menu

--- assets/js/logged-in.js
+++ assets/js/logged-in.js
@@ -1,10 +1,9 @@
 const userNameView = document.getElementById('user-name');
-const favList = document.querySelector('.fav-list');
+const favList = document.querySelector('.carousel-inner');
 const userContact = document.querySelector('.user-contact');
 const userAbout = document.querySelector('.user-about');
 const userIndex = document.querySelector('.user-index');
-
-
+const cards = document.querySelector(".cards");
 
 const urlParams = new URLSearchParams(window.location.search);
 const userName = urlParams.get('username');
@@ -18,6 +17,13 @@ document.addEventListener('DOMContentLoaded', () => {
     userContact.setAttribute('href', `user-contact.html?username=${userName}`);
     userAbout.setAttribute('href', `user-about.html?username=${userName}`);
     userIndex.setAttribute('href', `user-index.html?username=${userName}`);
+
+    let foodDB = indexedDB.open('foods', 1);
+
+    foodDB.onsuccess = function() {
+        db = foodDB.result;
+        displayMenu();
+    }
 
     function loadFavoritesFromDB(storeName, userName) {
         return new Promise( (resolve, reject) => {
@@ -55,9 +61,23 @@ document.addEventListener('DOMContentLoaded', () => {
         
                 favFood.onsuccess = function() {
                     if (favFood.result) {
-                        output = `<li class="nav-item">
-                        <a class="nav-link show" data-toggle="tab" href="#tab-${index + 1}">${favFood.result.foodName}</a>
-                            </li> `
+                        if (index == 0) {
+                        output += `<div class="carousel-item active">
+                        <img class="d-block w-100 img-fluid" src="${favFood.result.imgSrc}"
+                            alt="">
+                        <div class="carousel-caption d-md-block">
+                            <h5 class="carousel-foodName">${favFood.result.foodName}</h5>
+                        </div>
+                        </div>`;
+                        } else {
+                            output += `<div class="carousel-item">
+                            <img class="d-block w-100 img-fluid" src="${favFood.result.imgSrc}"
+                                alt="">
+                            <div class="carousel-caption d-md-block">
+                                <h5 class="carousel-foodName">${favFood.result.foodName}</h5>
+                            </div>
+                            </div>`;
+                        }
                         resolve(output);
                     } else {
                         reject(Error('No favorite data'));
@@ -88,20 +108,61 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     loadFavoritesFromDB('users', userName)
-    .then((response) => {
-        displayFavoritesFromDB('foods', response);
+    .then((favoriteArray) => {
+        displayFavoritesFromDB('foods', favoriteArray);
     })
-    // let userDB = indexedDB.open('users', 1);
-    // userDB.onsuccess = function() {
-    //     db = userDB.result;
-    //     getFavorites();
-    // } 
-    
-    // let foodDB = indexedDB.open('foods', 1);
-    // foodDB.onsuccess = function() {
-    //     dbFood = foodDB.result;
-    //     displayFavorites();
-    // }
+
+    function displayMenu() {
+
+        let foodStore = db.transaction('foods').objectStore('foods');
+  
+        foodStore.openCursor().onsuccess = function(e) {
+            let cursor = e.target.result;
+  
+            if (cursor) {
+  
+              let ratingStars = '';
+              for (let i = 1; i <= cursor.value.rating; i++){
+                ratingStars += '<i class="fa fa-star"></i> ';
+              }
+              for (let j = 5 - cursor.value.rating; j > 0; j--){
+                ratingStars += '<i class="fa fa-star-o"></i> ';
+              }
+  
+              let tags = '';
+              for (let k = 0; k < cursor.value.tag.length; k++){
+                tags += `<span>${cursor.value.tag[k]}</span>`;
+              } 
+  
+              
+              const foodCard = document.createElement("div");
+              foodCard.className = 'col-12 col-md-4 col-lg-3';
+              foodCard.innerHTML = `<div class="card" data-food-id="${cursor.value.id}" style="cursor: pointer;">
+                  <img src="${cursor.value.imgSrc}" class="card-img-top img-fluid" alt="">
+                  <div class="card-body">
+                      <h5 class="card-title">${cursor.value.foodName}</h5>
+                      <p class="rating">Rating
+                          <span class="rating-stars">
+                            ${ratingStars}
+                          </span>
+                      </p>
+                      <p class="price">Price
+                          <span class="price-amount">${cursor.value.price}.00</span>
+                      </p>
+                      <p class="tags">
+                            ${tags}  
+                      </p>
+                  </div>
+              </div>
+              <p class="text-center action-btns">
+                      <button class="heart-btn"><i class="fa fa-heart-o" aria-hidden="true"></i>
+                    </button>
+              </p>`;
+              cards.appendChild(foodCard);
+              cursor.continue();
+            }
+        }
+      };
     
     function getFavorites(db) {
         var transaction = db.transaction(['users']);
